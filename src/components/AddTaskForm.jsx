4030d9165394ad13db9e37842bb538722253af21
narodstream <vlad_vorobyev@mail.ru>
2026-04-08 08:51:00 +0300

    commit05

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,11 +1,24 @@
 import Field from './Field'
 import Button from './Button'
 
-const AddTaskForm = () => {
+const AddTaskForm = (props) => {
+  const {
+    addTask,
+  } = props
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    addTask()
+  }
+
   return (
-    <form className="todo__form">
-      <Field />
-      <Button />
+    <form className="todo__form" onSubmit={onSubmit}>
+      <Field
+        className="todo__field"
+        label="New task title"
+        id="new-task"
+      />
+      <Button type="submit">Add</Button>
     </form>
   )
 }
--- src/components/TodoInfo.jsx
+++ src/components/TodoInfo.jsx
@@ -1,8 +1,26 @@
-const TodoInfo = () => {
+const TodoInfo = (props) => {
+  const {
+    total,
+    done,
+    onDeleteAllButtonClick,
+  } = props
+
+  const hasTasks = total > 0
+
   return (
     <div className="todo__info">
-      <div className="todo__total-tasks">Total tasks: <span>0</span></div>
-      <button className="todo__delete-all-button" type="button">Delete all</button>
+      <div className="todo__total-tasks">
+        Done {done} from {total}
+      </div>
+      {hasTasks && (
+        <button
+          className="todo__delete-all-button"
+          type="button"
+          onClick={onDeleteAllButtonClick}
+        >
+          Delete all
+        </button>
+      )}
     </div>
   )
 }
